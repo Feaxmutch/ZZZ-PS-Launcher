7b173fa6dfe3b23f85987805152ec55c8f5d30b5
Feaxmutch <118442920+Feaxmutch@users.noreply.github.com>
2026-06-21 20:31:29 +0300

    chore: server version in CommitData

--- Profile/CommitData.cs
+++ Profile/CommitData.cs
@@ -2,13 +2,16 @@
 {
     public class CommitData
     {
-        public CommitData(string name, string value)
+        public CommitData(string name, string clientVersion, string value)
         {
             Name = name;
+            ClientVersion = clientVersion;
             Value = value;
         }
 
         public string Name { get; }
+
+        public string ClientVersion { get; }
 
         public string Value { get; }
     }
